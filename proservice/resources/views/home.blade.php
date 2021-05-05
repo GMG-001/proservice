3494ce4046b412936bdafacf310447b05fb1f83e
giorgi <giorgimegrelishvili45@gmail.com>
2021-05-04 20:33:19 -0500

    finish

--- proservice/resources/views/home.blade.php
+++ proservice/resources/views/home.blade.php
@@ -32,7 +32,7 @@
 </header>
 <section class="section_1">
     <div class="border_1">
-        <div class="z-index">
+        <div class="z-index container">
             <h1>MARTINI</h1>
             <nav class="nav_1">
                 <ul class="section__list">
@@ -43,7 +43,7 @@
                     <li><a href="#">გლემპინგი</a></li>
                 </ul>
             </nav>
-            <div class="section__list">
+            <div class="section__list auth_regis">
                 <a href="#" class="log_in">ავტორიზაცია</a><br>
                 <a href="#" style="color: #54E1BC; " class="reg">რეგისტრაცია</a>
             </div>
@@ -52,15 +52,19 @@
             <h2>RESERVATION</h2>
             <div class="reservation_inputs">
                 <label class="reserv__text" for="Check-in">Check-in date</label>
-                <input type="date" class="data" id="date">
+                <!-- <input type="date" class="data data_placeholder" id="date"> -->
+                <input type="text" placeholder="Check-in date" class="data data_placeholder" id="date" onfocus="(this.type='date')">
+
                 <label class="reserv__text" for="Check-out">Check-out date</label>
-                <input type="date" class="data" id="date1">
+                <input type="text" placeholder="Check-in date" class="data data_placeholder1" id="date" onfocus="(this.type='date')">
+                <!-- <input type="date" class="data data_placeholder1" id="date1"> -->
 
-                <input type="number" class="data">
-
-                <input class="inline__input" type="number">
-
-                <input  class="inline__input" type="number">
+                <label class="reserv__text" for="Check-in">ზრდასრული</label>
+                <input type="number" class="data" min="1" placeholder="ადამიანი">
+                <label class="reserv__text sm_in_lb" for="data">ბავშვები</label>
+                <label class="reserv__text" for="Check-in">ოთახი</label>
+                <input class="inline__input" type="number" min="0" placeholder="ბავშვები">
+                <input  class="inline__input" type="number" min="1" placeholder="ოთახი">
                 <button type="submit">ძიება</button>
                 <script>
                     let today = new Date().toISOString().split('T')[0];
@@ -91,9 +95,34 @@
         </p>
     </div>
     <div class="contact">
-        <p>
-            დაგვიკავშირდით ნომერზე (995) 599 55 64 64 ან მოგვწერეთ info@martini.ge
-        </p>
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-item active">
+                <p class="slider_text">
+                    დაგვიკავშირდით ნომერზე (995dfdfdf) 599 55 64 64 ან მოგვწერეთ info@martini.ge
+                </p>
+            </div>
+            <div class="carousel-item">
+                <p class="slider_text">
+                    დაგვიკავშირდით ნომერზე (99df235) 599 55 64 64 ან მოგვწერეთ info@martini.ge
+                </p>
+            </div>
+            <div class="carousel-item">
+                <p class="slider_text">
+                    დაგვიკავშირდით ნომერზე (995) 599 55 64 64 ან მოგვწერეთ info@martini.ge
+                </p>
+            </div>
+        </div>
+        <div class="container slider_button_div">
+            <button class="slider_button" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="slider_button" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
     </div>
 
 </section>
@@ -117,7 +146,7 @@
 </section>
 <footer>
     <div class="border">
-        <p class="footer_p1">
+        <p class="container">
             © 2021 Hotel Martini
         </p>
         <div class="icon">
@@ -125,7 +154,7 @@
             <svg class="inst" xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 127.999 128"><path fill="#231f20" d="M64.053 75.321c6.188 0 11.207-4.861 11.207-10.855 0-6-5.02-10.857-11.207-10.857-6.186 0-11.203 4.857-11.203 10.857 0 5.993 5.017 10.855 11.203 10.855zM76.748 53.924h6.305a2.52 2.52 0 002.512-2.514v-6.009a2.522 2.522 0 00-2.512-2.516h-6.305a2.52 2.52 0 00-2.512 2.516v6.009a2.518 2.518 0 002.512 2.514z"/><path fill="#231f20" d="M81.398 65.072c0 9.276-7.768 16.802-17.346 16.802-9.576 0-17.342-7.525-17.342-16.802 0-1.667.254-3.271.719-4.79h-5.123V83.85c0 1.221.996 2.213 2.217 2.213h38.848a2.218 2.218 0 0 0 2.219-2.213V60.282h-4.91c.466 1.519.718 3.123.718 4.79z"/><path fill="#231f20" d="M63.998 7.989c-30.883 0-56.008 25.126-56.008 56.01 0 30.885 25.125 56.012 56.008 56.012 30.885 0 56.012-25.127 56.012-56.012 0-30.884-25.127-56.01-56.012-56.01zm27.898 77.33c0 3.938-3.223 7.158-7.16 7.158H43.268c-3.938 0-7.162-3.22-7.162-7.158V43.852c0-3.94 3.225-7.162 7.162-7.162h41.469c3.938 0 7.16 3.222 7.16 7.162v41.467z"/></svg>
             <svg class="fb" xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 127.999 128"><path fill="#231f20" d="M64 7.989c-30.885 0-56.01 25.126-56.01 56.01 0 30.885 25.125 56.012 56.01 56.012 30.883 0 56.01-25.127 56.01-56.012 0-30.884-25.127-56.01-56.01-56.01zm15.83 35.358a1 1 0 0 1-1 1l-6.367.004c-4.201 0-4.969 1.636-4.969 4.858v6.684h10.92c.287 0 .561.124.75.34s.277.503.242.788l-1.555 12.033a1 1 0 0 1-.992.872h-9.365v29.879a1 1 0 0 1-1 1H54.07a1 1 0 0 1-1-1V69.926h-9.395a1 1 0 0 1-1-1V56.893a1 1 0 0 1 1-1h9.395v-7.875c0-10.43 6.312-16.908 16.475-16.908 4.369 0 8.23.325 9.416.482a1 1 0 0 1 .869.991v10.764z"/></svg>
         </div>
-        <p class="footer_p2">
+        <p class="container footer_p2">
             Created by ProService
         </p>
     </div>
